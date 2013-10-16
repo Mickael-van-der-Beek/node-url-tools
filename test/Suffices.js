9670a1b4db66ebc758a39b63674ca398bfb1bec6
Nils De Moor <nils.de.moor@gmail.com>
2013-10-16 18:11:27 +0200

    added Suffices test

---
+++ test/Suffices.js
@@ -0,0 +1,91 @@
+var Suffices = require('../lib/Suffices.js');
+
+module.exports.SufficesTest = {
+  testNewline: function(test) {
+    test.deepEqual(new Suffices("").tree, {});
+    test.deepEqual(new Suffices("  \n \n  ").tree, {});
+    test.deepEqual(new Suffices("\n\n").tree, {});
+    test.done();
+  },
+  testComments: function(test) {
+    test.deepEqual(new Suffices("// foo").tree, {});
+    test.deepEqual(new Suffices("// foo\n// bar\n\n//baz").tree, {});
+    test.deepEqual(new Suffices("////\/").tree, {});
+    test.done();
+  },
+  testSimple: function(test) {
+    test.deepEqual(new Suffices("a").tree, { "a" : { true : true } });
+    test.deepEqual(new Suffices("a\na").tree, { "a" : { true : true } });
+    test.deepEqual(new Suffices("b.a").tree, { "a" : { "b" : { true : true } } });
+    test.deepEqual(new Suffices("d.c.b.a").tree, { "a" : { "b" : { "c" : { "d" : { true : true } } } } });
+    test.deepEqual(new Suffices("*.c.b.a").tree, { "a" : { "b" : { "c" : { "*" : { true : true } } } } });
+    test.deepEqual(new Suffices("!d.c.b.a").tree, { "a" : { "b" : { "c" : { "!d" : { true : true } } } } });
+    test.done();
+  },
+  testComplex: function(test) {
+    test.deepEqual(
+      new Suffices("a\nb.a").tree, 
+      { 
+        "a" : { 
+          "b" : { 
+            true : true 
+          }, 
+          true : true 
+        } 
+      }
+    );
+    test.deepEqual(
+      new Suffices("a\nb.a\nc1.b.a\nc2.b.a").tree, 
+      { 
+        "a" : { 
+          "b" : { 
+            true : true, 
+            "c1" : { true : true }, 
+            "c2" : { true : true } 
+          }, 
+        true : true } 
+      }
+    );
+    test.deepEqual(
+      new Suffices("a\nc1.b.a\nc2.b.a").tree, 
+      { 
+        "a" : { 
+          "b" : { 
+            "c1" : { true : true }, 
+            "c2" : { true : true } 
+          }, 
+        true : true } 
+      }
+    );
+    test.deepEqual(
+      new Suffices("a\nc1.b.a\nc2.b.a\n*.c1.b.a").tree, 
+      { 
+        "a" : { 
+          "b" : { 
+            "c1" : { 
+              true : true, 
+              "*" : { true : true } 
+            }, 
+            "c2" : { true : true } 
+          }, 
+        true : true } 
+      }
+    );
+    test.deepEqual(
+      new Suffices("a\nc1.b.a\nc2.b.a\n*.c1.b.a\n!d.c1.b.a").tree, 
+      { 
+        "a" : { 
+          "b" : { 
+            "c1" : { 
+              true : true, 
+              "*" : { true : true },
+              "!d": { true : true } 
+            }, 
+            "c2" : { true : true } 
+          }, 
+        true : true } 
+      }
+    );
+    test.done();
+  }
+}
